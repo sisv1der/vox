c1dc72ecc12dedf6c8f87aa1a0791259d1cc81b6
Ivan Yarigo <yarigo2018@yandex.ru>
2026-07-15 20:46:53 +0300

    chore: configure frontend development environment
    - add prettier
    - remove project template files
    - add OIDC library (react-oidc-context)

---
+++ frontend/vox/src/Vox.tsx
@@ -0,0 +1,7 @@
+import './App.css'
+
+const Vox = () => {
+  return <></>
+}
+
+export default Vox
--- frontend/vox/src/main.tsx
+++ frontend/vox/src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import Vox from './Vox.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Vox />
   </StrictMode>,
 )
